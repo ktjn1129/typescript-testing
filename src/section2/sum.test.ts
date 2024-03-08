c7f34ee3dab4681ba6fe0f3b85f1b1dd51e1eba8
ktjn1129 <ktjn1129@gmail.com>
2024-03-08 17:50:47 +0900

    format

--- src/section2/sum.test.ts
+++ src/section2/sum.test.ts
@@ -1,15 +1,13 @@
 import { sum } from "./sum";
 
 it("1と2を足すと3になる", () => {
-    expect(sum(1, 2)).toBe(3);
+  expect(sum(1, 2)).toBe(3);
 });
 
 it.each`
-a | b | expected
-${1} | ${2} | ${3}
-${1} | ${-2} | ${-1}
+  a    | b     | expected
+  ${1} | ${2}  | ${3}
+  ${1} | ${-2} | ${-1}
 `("$aと$bを足すと$expectedになる", ({ a, b, expected }) => {
-    expect(sum(a, b)).toBe(expected);
+  expect(sum(a, b)).toBe(expected);
 });
-
-
